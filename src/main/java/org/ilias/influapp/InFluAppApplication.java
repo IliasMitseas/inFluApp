cec972984bba20f10c1fa9f28219f1be46b967df
ilias.mitseas <ilias.mitseas@gmail.com>
2025-11-22 12:36:42 +0200

    initial commit

---
+++ src/main/java/org/ilias/influapp/InFluAppApplication.java
@@ -0,0 +1,13 @@
+package org.ilias.influapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InFluAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InFluAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ilias/influapp/InFluAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ilias.influapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InFluAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
