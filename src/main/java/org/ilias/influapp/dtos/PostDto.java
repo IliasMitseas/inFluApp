1f7bcad6ad0661165c5dd027ea8c94ce6dde5e8a
ilias.mitseas <ilias.mitseas@gmail.com>
2026-03-05 15:34:15 +0200

    forms validation

--- src/main/java/org/ilias/influapp/dtos/PostDto.java
+++ src/main/java/org/ilias/influapp/dtos/PostDto.java
@@ -1,5 +1,7 @@
 package org.ilias.influapp.dtos;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +15,14 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostDto {
+    @NotBlank(message = "Content is required")
     private String content;
     private List<Reaction> reactions;
     private List<String> comments;
+    @Min(value = 0, message = "Shares cannot be negative")
     private Integer shares;
+    @Min(value = 0, message = "Reach cannot be negative")
     private Integer reach;
+    @Min(value = 0, message = "Impressions cannot be negative")
     private Integer impressionCount;
 }
--- src/main/java/org/ilias/influapp/entities/CountsRequest.java
+++ src/main/java/org/ilias/influapp/entities/CountsRequest.java
@@ -1,13 +1,20 @@
 package org.ilias.influapp.entities;
 
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 public class CountsRequest {
+    @Min(value = 0, message = "Like count cannot be negative")
     private Integer likeCount = 0;
+    @Min(value = 0, message = "Love count cannot be negative")
     private Integer loveCount = 0;
+    @Min(value = 0, message = "Haha count cannot be negative")
     private Integer hahaCount = 0;
+    @Min(value = 0, message = "Wow count cannot be negative")
     private Integer wowCount = 0;
+    @Min(value = 0, message = "Sad count cannot be negative")
     private Integer sadCount = 0;
+    @Min(value = 0, message = "Angry count cannot be negative")
     private Integer angryCount = 0;
 }
